010130c30619e76b1247abee914321febe5f2711
Haxor1991 <asamitalex@gmail.com>
2019-07-09 00:23:04 +0530

    First commit from desktop

---
+++ JenkinsPush/src/Hello.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author amits
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Hello World...."+i);
+			
+		}
+
+	}
+
+}
